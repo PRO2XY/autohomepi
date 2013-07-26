a788ce12184b4615533894161b55f43f0b1a6c76
PRO2XY <pranavsharma2504@gmail.com>
2013-07-27 00:37:36 +0530

    Initial commit

---
+++ autohomepiserver.py
@@ -0,0 +1,10 @@
+# Script created by Pranav Sharma (pranavsharma2504@gmail.com)
+# Initial version created on 27th July 2013 : 00:18 IST
+# Current Version: 0.10.1
+# Version Date: 27-07-2013 00:32
+
+### USER EDITABLES
+DATABASEUSERNAME = "root"
+DATABASEPASSWORD = "root"
+REFRESHTIME = 5 #seconds
+### USER EDITABLES END
--- config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhost = "localhost";
-$dbname = "autohomepi";
-$dbusername = "root";
-$dbpassword = "root";
-$errormsg = "There was some error processing your request. This has been logged and our engineers are working on resolving this issue. Please try later...";
-?>
