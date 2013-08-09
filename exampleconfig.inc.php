9824e381cb2789d5047d5cdd38146d52fe647ed7
PRO2XY <pranavsharma2504@gmail.com>
2013-08-09 14:36:59 +0530

    OpenRemote user validation in process

--- exampleconfig.inc.php
+++ exampleconfig.inc.php
@@ -4,4 +4,7 @@ $dbname = "autohomepi";
 $dbusername = "root";
 $dbpassword = "root";
 $errormsg = "There was some error processing your request. This has been logged and our engineers are working on resolving this issue. Please try later...";
+$ORusername = ""; //Username for OpenRemote
+$ORpassword = ""; //Password for OpenRemote
+
 ?>
--- remoteprocessor.php
+++ remoteprocessor.php
@@ -2,27 +2,31 @@
 
 include 'config.inc.php';
 
-if (isset($_POST['switch_id'])) {
-    $switch_id = $_POST['switch_id'];
-    if (isset($_POST['switch_status'])) {
-        $switch_status = $_POST['switch_status'];
-        $switch_status = $switch_status ? "on" : "off";
-        // Write Switch
+if (isset($_POST['username']) && isset($_POST['password']) && $_POST['username'] == $ORusername && $_POST['password'] == $ORpassword) {
+    if (isset($_POST['switch_id'])) {
+        $switch_id = $_POST['switch_id'];
+        if (isset($_POST['switch_status'])) {
+            $switch_status = $_POST['switch_status'];
+            $switch_status = $switch_status ? "on" : "off";
+            // Write Switch
+            $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die();
+            $query = "UPDATE `$autohomepi_db_dbname`.`switches` SET `switch_state`=\"" . $switch_status . "\" WHERE `switch_id` = '$switch_id'";
+            $result = mysqli_query($con, $query) or die();
+            mysqli_close($con);
+            echo $result;
+        }
+    } else if (isset($_GET['switch_id'])) {
+        // Read Switch
         $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die();
-        $query = "UPDATE `$autohomepi_db_dbname`.`switches` SET `switch_state`=\"" . $switch_status . "\" WHERE `switch_id` = '$switch_id'";
+        $query = "SELECT `switch_state` FROM `$autohomepi_db_dbname`.`switches` WHERE `switch_id` = '$switch_id'";
         $result = mysqli_query($con, $query) or die();
+        $row = mysqli_fetch_row($result);
+        $switch_status = $row[0];
         mysqli_close($con);
-        echo $result;
+        echo $switch_status;
+    } else {
+        echo 'Error: 404 - Page not found!';
     }
-} else if (isset($_GET['switch_id'])) {
-    // Read Switch
-    $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die();
-    $query = "SELECT `switch_state` FROM `$autohomepi_db_dbname`.`switches` WHERE `switch_id` = '$switch_id'";
-    $result = mysqli_query($con, $query) or die();
-    $row = mysqli_fetch_row($result);
-    $switch_status = $row[0];
-    mysqli_close($con);
-    echo $switch_status;
 } else {
     echo 'Error: 404 - Page not found!';
 }
