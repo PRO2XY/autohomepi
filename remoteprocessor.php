2e77cb53975c81a6964ddf3b7de9566b4d25b794
PRO2XY <pranavsharma2504@gmail.com>
2013-08-10 23:02:18 +0530

    Corrected wrong variable names for database access

--- remoteprocessor.php
+++ remoteprocessor.php
@@ -1,8 +1,7 @@
 <?php
-
 include 'config.inc.php';
 
-if (isset($_POST['username']) && isset($_POST['password']) && $_POST['username'] == $ORusername && $_POST['password'] == $ORpassword) {
+//if (isset($_POST['username']) && isset($_POST['password']) && $_POST['username'] == $ORusername && $_POST['password'] == $ORpassword) {
     if (isset($_POST['switch_id'])) {
         $switch_id = $_POST['switch_id'];
         if (isset($_POST['switch_status'])) {
@@ -10,15 +9,16 @@ if (isset($_POST['username']) && isset($_POST['password']) && $_POST['username']
             $switch_status = $switch_status ? "on" : "off";
             // Write Switch
             $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die();
-            $query = "UPDATE `$autohomepi_db_dbname`.`switches` SET `switch_state`=\"" . $switch_status . "\" WHERE `switch_id` = '$switch_id'";
+            $query = "UPDATE `$dbname`.`switches` SET `switch_state`=\"" . $switch_status . "\" WHERE `switch_id` = '$switch_id'";
             $result = mysqli_query($con, $query) or die();
             mysqli_close($con);
             echo $result;
         }
     } else if (isset($_GET['switch_id'])) {
         // Read Switch
+	$switch_id = $_GET['switch_id'];
         $con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname) or die();
-        $query = "SELECT `switch_state` FROM `$autohomepi_db_dbname`.`switches` WHERE `switch_id` = '$switch_id'";
+        $query = "SELECT `switch_state` FROM `$dbname`.`switches` WHERE `switch_id` = '$switch_id'";
         $result = mysqli_query($con, $query) or die();
         $row = mysqli_fetch_row($result);
         $switch_status = $row[0];
@@ -27,7 +27,7 @@ if (isset($_POST['username']) && isset($_POST['password']) && $_POST['username']
     } else {
         echo 'Error: 404 - Page not found!';
     }
-} else {
-    echo 'Error: 404 - Page not found!';
-}
+//} else {
+//    echo 'Error: 404 - Page not found!';
+//}
 ?>
